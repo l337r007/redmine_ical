d14ebff087110a16f290cdbc64fbdf76f96fd9af
Wolfgang Herget <wolfgang.herget@dfki.de>
2013-10-10 15:57:24 +0200

    safeguard Version monkey-patch

--- lib/redmine_ical/patch_redmine_classes.rb
+++ lib/redmine_ical/patch_redmine_classes.rb
@@ -145,7 +145,9 @@ end
 unless CalendarsController.included_modules.include?(::Plugin::Ical::CalendarsController)
     CalendarsController.send(:include, ::Plugin::Ical::CalendarsController)
 end
-Version.send(:include, ::Plugin::Ical::Version)
 unless Issue.included_modules.include?(::Plugin::Ical::Issue)
     Issue.send(:include, ::Plugin::Ical::Issue)
+end
+unless Version.included_modules.include?(::Plugin::Ical::Version)
+    Version.send(:include, ::Plugin::Ical::Version)
 end
